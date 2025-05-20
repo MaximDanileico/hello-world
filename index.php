a9a200fe453a842034cc22f5da88b535d7e2bbad
MDani <mdannileico@gmail.com>
2025-05-20 15:18:25 +0300

    add a base library

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Mdani\HelloWorld\Hello;
+
+echo Hello::world();
---
+++ src/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mdani\HelloWorld;
+
+class Hello
+{
+    public static function world()
+    {
+        return 'Hello, world!';
+    }
+}
